bad57b862e80102b5ad2a584154b86285ec9d8cc
Madalina Borzasi <borzasi.madalina@gmail.com>
2022-05-29 19:41:54 +0300

    Add navigation functionality

--- src/app/components/hero-table/hero-table.component.ts
+++ src/app/components/hero-table/hero-table.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero, HeroService } from '../../services/hero.service';
+import { DEFAULT_PAGE, Hero, HeroService } from '../../services/hero.service';
 
 @Component({
     selector: 'rx-hero-table',
     templateUrl: './hero-table.component.html',
     styleUrls: ['./hero-table.component.scss'],
 })
-export class HeroTableComponent implements OnInit {
-    heroes: Hero[];
+export class HeroTableComponent {
+    heroes$ = this.hero.heroes$;
+    search$ = this.hero.searchBS;
+    page$ = this.hero.userPage$;
+    limit$ = this.hero.limitBS;
+    totalResults$ = this.hero.totalResults$;
+    totalPages$ = this.hero.totalPages$;
 
-    constructor(public hero: HeroService) {
-        hero.heroes$.subscribe(heroes => {
-            this.heroes = heroes;
-        });
+    constructor(public hero: HeroService) {}
+
+    doSearch(event: any) {
+        this.hero.searchBS.next(event.target.value);
+        this.hero.pageBS.next(DEFAULT_PAGE);
     }
 
-    ngOnInit() {}
+    movePage(moveBy: number) {
+        const currentPage = this.hero.pageBS.getValue(); //syncronous
+        this.hero.pageBS.next(currentPage + moveBy);
+    }
+
+    setLimit(limit: number) {
+        this.hero.limitBS.next(limit);
+        this.hero.pageBS.next(DEFAULT_PAGE);
+    }
 }
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { debounceTime, map, shareReplay, switchMap } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 export interface Hero {
@@ -41,6 +41,9 @@ const LIMIT_LOW = 10;
 const LIMIT_MID = 25;
 const LIMIT_HIGH = 100;
 const LIMITS = [LIMIT_LOW, LIMIT_MID, LIMIT_HIGH];
+export const DEFAULT_SEARCH = '';
+export const DEFAULT_LIMIT = LIMIT_HIGH;
+export const DEFAULT_PAGE = 0;
 
 @Injectable({
     providedIn: 'root',
@@ -48,16 +51,48 @@ const LIMITS = [LIMIT_LOW, LIMIT_MID, LIMIT_HIGH];
 export class HeroService {
     limits = LIMITS;
 
-    heroes$: Observable<Hero[]> = this.http
-        .get(HERO_API, {
-            params: {
+    searchBS = new BehaviorSubject(DEFAULT_SEARCH);
+    limitBS = new BehaviorSubject(DEFAULT_LIMIT);
+    pageBS = new BehaviorSubject(DEFAULT_PAGE);
+
+    userPage$ = this.pageBS.pipe(map(page => page + 1));
+
+    params$ = combineLatest([this.searchBS, this.limitBS, this.pageBS]).pipe(
+        map(([searchTerm, limit, page]) => {
+            const params: any = {
                 apikey: environment.MARVEL_API.PUBLIC_KEY,
-                limit: `${LIMIT_LOW}`,
-                // nameStartsWith: 'iron', // once we have search
-                offset: `${0}`, // page * limit
-            },
-        })
-        .pipe(map((res: any) => res.data.results));
+                limit: `${limit}`,
+                //nameStartsWith: 'rock', // once we have search
+                offset: `${page * limit}`, // page * limit
+            };
+            if (searchTerm.length) {
+                params.nameStartsWith = searchTerm;
+            }
+            return params;
+        }),
+    );
+
+    private heroesRespose$ = this.params$.pipe(
+        debounceTime(500),
+        switchMap(incomingParams =>
+            this.http.get(HERO_API, {
+                params: incomingParams,
+            }),
+        ),
+        shareReplay(1),
+    );
+
+    totalResults$ = this.heroesRespose$.pipe(
+        map((response: any) => response.data.total),
+    );
+
+    heroes$ = this.heroesRespose$.pipe(
+        map((response: any) => response.data.results),
+    );
+
+    totalPages$ = combineLatest([this.totalResults$, this.limitBS]).pipe(
+        map(([totalResults, limit]) => Math.ceil(totalResults / limit)),
+    );
 
     constructor(private http: HttpClient) {}
 }
